5a6fef4f98e43351106495f7465774ab8e40b3e8
Alex Reznichenko <alex.reznichenko789@gmail.com>
2019-10-24 08:38:26 +0300

    Add tasks query to api

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NextPage } from "next";
 import { withApollo } from "../lib/apollo";
+import gql from "graphql-tag";
+import { Query, useQuery } from "react-apollo";
 
 interface InitialProps {
   greatting: string;
@@ -8,8 +10,37 @@ interface InitialProps {
 
 interface Props extends InitialProps {}
 
+const tasksQuery = gql`
+  query Tasks($status: TaskStatus) {
+    tasks(status: $status) {
+      id
+      title
+      status
+    }
+  }
+`;
+
+interface TasksQuery {
+  tasks: {
+    id: number;
+    title: string;
+    status: string;
+  }[];
+}
+
 const IndexPage: NextPage<Props, InitialProps> = props => {
-  return <div>{props.greatting}</div>;
+  const { loading, error, data } = useQuery(tasksQuery);
+
+  if (loading) return <p>Loading ...</p>;
+  else if (error) return <p>An error occur</p>;
+  const tasks = data && data.tasks ? data.tasks : [];
+  return (
+    <ul>
+      {tasks.map(task => {
+        return <li key={task.id}>{task.title}</li>;
+      })}
+    </ul>
+  );
 };
 
 IndexPage.getInitialProps = async () => {
